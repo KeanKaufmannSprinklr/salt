1f9bbc7b39d5494ed6a5216418f1af98816c5047
f.zipser@gmx.de <f.zipser@gmx.de>
2015-10-02 15:42:15 +0200

    now added extension of SaltNotificationFactory

--- salt-extensions/src/main/java/de/hu_berlin/u/saltnpepper/salt/extensions/notification/SaltNotificationFactory.java
+++ salt-extensions/src/main/java/de/hu_berlin/u/saltnpepper/salt/extensions/notification/SaltNotificationFactory.java
@@ -4,8 +4,6 @@ import java.util.Collection;
 import java.util.List;
 
 import de.hu_berlin.u.saltnpepper.graph.Graph;
-import de.hu_berlin.u.saltnpepper.graph.IdentifiableElement;
-import de.hu_berlin.u.saltnpepper.graph.Identifier;
 import de.hu_berlin.u.saltnpepper.graph.Label;
 import de.hu_berlin.u.saltnpepper.graph.Layer;
 import de.hu_berlin.u.saltnpepper.graph.Node;
@@ -30,8 +28,24 @@ import de.hu_berlin.u.saltnpepper.salt.common.STextualRelation;
 import de.hu_berlin.u.saltnpepper.salt.common.STimeline;
 import de.hu_berlin.u.saltnpepper.salt.common.STimelineRelation;
 import de.hu_berlin.u.saltnpepper.salt.common.SToken;
-import de.hu_berlin.u.saltnpepper.salt.common.SaltProject;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SCorpusDocumentRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SCorpusGraphImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SCorpusImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SCorpusRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SDocumentGraphImpl;
 import de.hu_berlin.u.saltnpepper.salt.common.impl.SDocumentImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SDominanceRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SMedialDSImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SMedialRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SOrderRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SPointingRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SSpanImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SSpanningRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.SStructureImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.STextualDSImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.STextualRelationImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.STimelineImpl;
+import de.hu_berlin.u.saltnpepper.salt.common.impl.STimelineRelationImpl;
 import de.hu_berlin.u.saltnpepper.salt.common.impl.STokenImpl;
 import de.hu_berlin.u.saltnpepper.salt.core.SAnnotation;
 import de.hu_berlin.u.saltnpepper.salt.core.SFeature;
@@ -41,7 +55,18 @@ import de.hu_berlin.u.saltnpepper.salt.core.SMetaAnnotation;
 import de.hu_berlin.u.saltnpepper.salt.core.SNode;
 import de.hu_berlin.u.saltnpepper.salt.core.SProcessingAnnotation;
 import de.hu_berlin.u.saltnpepper.salt.core.SRelation;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SFeatureImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SGraphImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SLayerImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SMetaAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SNodeImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SProcessingAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.core.impl.SRelationImpl;
 import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.Notifier;
+import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.impl.GraphNotifierImpl;
+import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.impl.LabelNotifierImpl;
+import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.impl.LayerNotifierImpl;
 import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.impl.NodeNotifierImpl;
 import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.impl.NotifierHelper;
 import de.hu_berlin.u.saltnpepper.salt.extensions.notification.graph.impl.RelationNotifierImpl;
@@ -52,8 +77,14 @@ import de.hu_berlin.u.saltnpepper.salt.semantics.SPOSAnnotation;
 import de.hu_berlin.u.saltnpepper.salt.semantics.SSentenceAnnotation;
 import de.hu_berlin.u.saltnpepper.salt.semantics.STypeAnnotation;
 import de.hu_berlin.u.saltnpepper.salt.semantics.SWordAnnotation;
+import de.hu_berlin.u.saltnpepper.salt.semantics.impl.SCatAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.semantics.impl.SLemmaAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.semantics.impl.SPOSAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.semantics.impl.SSentenceAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.semantics.impl.STypeAnnotationImpl;
+import de.hu_berlin.u.saltnpepper.salt.semantics.impl.SWordAnnotationImpl;
 
-public class SaltNotificationFactory extends SaltFactoryImpl implements ISaltFactory, Notifier{
+public class SaltNotificationFactory extends SaltFactoryImpl implements ISaltFactory, Notifier {
 
 	// ==========================================> listener list
 	protected static List<Listener> listenerList = null;
@@ -69,7 +100,7 @@ public class SaltNotificationFactory extends SaltFactoryImpl implements ISaltFac
 	public synchronized void addListener(Listener listener) {
 		listenerList = NotifierHelper.addListener(listenerList, listener);
 	}
-	
+
 	/** {@inheritDoc} **/
 	@Override
 	public void addListener(Collection<Listener> listener) {
@@ -84,28 +115,213 @@ public class SaltNotificationFactory extends SaltFactoryImpl implements ISaltFac
 
 	// ==========================================< listener list
 
+	// ==========================================> salt graph
+
 	@Override
 	public Node createNode() {
-		NodeNotifierImpl node= new NodeNotifierImpl();
+		NodeNotifierImpl node = new NodeNotifierImpl();
 		node.addListener(getListener());
-		return(node);
+		return (node);
 	}
 
 	@Override
 	public Relation<Node, Node> createRelation() {
-		RelationNotifierImpl<Node, Node> relation= new RelationNotifierImpl<Node, Node>();
+		RelationNotifierImpl<Node, Node> relation = new RelationNotifierImpl<Node, Node>();
 		relation.addListener(relation.getListener());
-		return(relation);
+		return (relation);
 	}
 
+	@Override
+	public Graph<Node, Relation<Node, Node>, Layer<Node, Relation<Node, Node>>> createGraph() {
+		GraphNotifierImpl<Node, Relation<Node, Node>, Layer<Node, Relation<Node, Node>>> graph = new GraphNotifierImpl();
+		graph.addListener(getListener());
+		return (graph);
+	}
+
+	@Override
+	public Label createLabel() {
+		LabelNotifierImpl label = new LabelNotifierImpl();
+		label.addListener(getListener());
+		return (label);
+	}
+
+	@Override
+	public Layer<Node, Relation<Node, Node>> createLayer() {
+		LayerNotifierImpl<Node, Relation<Node, Node>> node = new LayerNotifierImpl();
+		node.addListener(getListener());
+		return (node);
+	}
+
+	// ==========================================< salt graph
+	// ==========================================> salt core
+	@Override
+	public SGraph createSGraph() {
+		return (new SGraphImpl(createGraph()));
+	}
+
+	@Override
+	public SNode createSNode() {
+		return (new SNodeImpl(createNode()));
+	}
+
+	@Override
+	public SRelation<SNode, SNode> createSRelation() {
+		return (new SRelationImpl<SNode, SNode>(createRelation()));
+	}
+
+	@Override
+	public SAnnotation createSAnnotation() {
+		return (new SAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SMetaAnnotation createSMetaAnnotation() {
+		return (new SMetaAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SProcessingAnnotation createSProcessingAnnotation() {
+		return (new SProcessingAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SFeature createSFeature() {
+		return (new SFeatureImpl(createLabel()));
+	}
+
+	@Override
+	public SLayer createSLayer() {
+		return (new SLayerImpl(createLayer()));
+	}
+
+	// ==========================================< salt core
+	// ==========================================> salt common
+	@Override
+	public SCorpus createSCorpus() {
+		return (new SCorpusImpl(createNode()));
+	}
+
+	@Override
+	public SCorpusRelation createSCorpusRelation() {
+		return (new SCorpusRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SCorpusDocumentRelation createSCorpusDocumentRelation() {
+		return (new SCorpusDocumentRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SCorpusGraph createSCorpusGraph() {
+		return (new SCorpusGraphImpl(createGraph()));
+	}
+
+	@Override
+	public SDocumentGraph createSDocumentGraph() {
+		return (new SDocumentGraphImpl(createGraph()));
+	}
 
 	@Override
 	public SDocument createSDocument() {
-		return(new SDocumentImpl(createNode()));
+		return (new SDocumentImpl(createNode()));
 	}
-	
+
 	@Override
 	public SToken createSToken() {
-		return(new STokenImpl(createNode()));
+		return (new STokenImpl(createNode()));
 	}
+
+	@Override
+	public SSpanningRelation createSSpanningRelation() {
+		return (new SSpanningRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SDominanceRelation createSDominanceRelation() {
+		return (new SDominanceRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SPointingRelation createSPointingRelation() {
+		return (new SPointingRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SOrderRelation createSOrderRelation() {
+		return (new SOrderRelationImpl(createRelation()));
+	}
+
+	@Override
+	public STextualRelation createSTextualRelation() {
+		return (new STextualRelationImpl(createRelation()));
+	}
+
+	@Override
+	public STimelineRelation createSTimelineRelation() {
+		return (new STimelineRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SMedialRelation createSMedialRelation() {
+		return (new SMedialRelationImpl(createRelation()));
+	}
+
+	@Override
+	public SSpan createSSpan() {
+		return (new SSpanImpl(createNode()));
+	}
+
+	@Override
+	public SStructure createSStructure() {
+		return (new SStructureImpl(createNode()));
+	}
+
+	@Override
+	public STextualDS createSTextualDS() {
+		return (new STextualDSImpl(createNode()));
+	}
+
+	@Override
+	public SMedialDS createSMedialDS() {
+		return (new SMedialDSImpl(createNode()));
+	}
+
+	@Override
+	public STimeline createSTimeline() {
+		return (new STimelineImpl(createNode()));
+	}
+
+	// ==========================================< salt common
+	// ==========================================> salt semantics
+
+	@Override
+	public SCatAnnotation createSCatAnnotation() {
+		return (new SCatAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SPOSAnnotation createSPOSAnnotation() {
+		return (new SPOSAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SLemmaAnnotation createSLemmaAnnotation() {
+		return (new SLemmaAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public STypeAnnotation createSTypeAnnotation() {
+		return (new STypeAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SWordAnnotation createSWordAnnotation() {
+		return (new SWordAnnotationImpl(createLabel()));
+	}
+
+	@Override
+	public SSentenceAnnotation createSSentenceAnnotation() {
+		return (new SSentenceAnnotationImpl(createLabel()));
+	}
+	// ==========================================< salt semantics
 }
